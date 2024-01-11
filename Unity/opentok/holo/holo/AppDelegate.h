6dead05b50dd66710f090f2d0014ef6aed7809b4
Jose Antonio Olivera Ortega <joseantonio.oliveraortega@vonage.com>
2024-01-11 22:40:35 +0100

    OW-792: Build iOS test app

--- Unity/opentok/holo/holo/AppDelegate.h
+++ Unity/opentok/holo/holo/AppDelegate.h
@@ -1,7 +1,17 @@
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+#include <UnityFramework/UnityFramework.h>
+#include <UnityFramework/NativeCallProxy.h>
 
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UnityFrameworkListener>
+
+@property UnityFramework* ufw;
+@property bool didQuit;
+
+- (void)initUnity;
 
 @end
 
+extern int gArgc;
+extern char** gArgv;
+extern UnityFramework *gUfw;
