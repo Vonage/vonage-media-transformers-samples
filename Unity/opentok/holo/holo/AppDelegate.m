8f3585e976a06ac9ad556b2e057fc189dfe2a5bd
Jose Antonio Olivera Ortega <joseantonio.oliveraortega@vonage.com>
2024-01-11 22:40:35 +0100

    OW-792: Build iOS test app

--- Unity/opentok/holo/holo/AppDelegate.m
+++ Unity/opentok/holo/holo/AppDelegate.m
@@ -1,5 +1,28 @@
 #import "AppDelegate.h"
 
+int gArgc = 0;
+char** gArgv = nullptr;
+NSDictionary* appLaunchOpts;
+UnityFramework* gUfw;
+
+UnityFramework* UnityFrameworkLoad()
+{
+    NSString* bundlePath = nil;
+    bundlePath = [[NSBundle mainBundle] bundlePath];
+    bundlePath = [bundlePath stringByAppendingString: @"/Frameworks/UnityFramework.framework"];
+
+    NSBundle* bundle = [NSBundle bundleWithPath: bundlePath];
+    if ([bundle isLoaded] == false) [bundle load];
+
+    UnityFramework* ufw = [bundle.principalClass getInstance];
+    if (![ufw appController])
+    {
+        // unity is not initialized
+        [ufw setExecuteHeader: &_mh_execute_header];
+    }
+    return ufw;
+}
+
 @interface AppDelegate ()
 
 @end
@@ -9,6 +32,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    appLaunchOpts = launchOptions;
+    [self initUnity];
     return YES;
 }
 
@@ -29,5 +54,62 @@
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
+- (bool)unityIsInitialized { return [self ufw] && [[self ufw] appController]; }
+
+- (void)initUnity
+{
+    if([self unityIsInitialized]) {
+        NSLog(@"Unity already initialized. Unload Unity first");
+        return;
+    }
+
+    if([self didQuit]) {
+        NSLog(@"Unity cannot be initialized after quit. Use unload instead");
+        return;
+    }
+
+    [self setUfw: UnityFrameworkLoad()];
+    gUfw = self.ufw;
+
+    // Set UnityFramework target for Unity-iPhone/Data folder to make Data part of a UnityFramework.framework and uncomment call to setDataBundleId
+    // ODR is not supported in this case, ( if you need embedded and ODR you need to copy data )
+    [[self ufw] setDataBundleId: "com.unity3d.framework"];
+    [[self ufw] registerFrameworkListener: self];
+
+    [[self ufw] runEmbeddedWithArgc: gArgc argv: gArgv appLaunchOpts: appLaunchOpts];
+    [self.ufw.appController.window setHidden:YES];
+}
+
+- (void)unloadButtonTouched:(UIButton *)sender
+{
+    if(![self unityIsInitialized]) {
+        NSLog(@"Unity is not initialized. Initialize Unity first");
+    } else {
+        [UnityFrameworkLoad() unloadApplication];
+    }
+}
+
+- (void)unityDidUnload:(NSNotification*)notification
+{
+    NSLog(@"unityDidUnload called");
+
+    [[self ufw] unregisterFrameworkListener: self];
+    [self setUfw: nil];
+}
+
+- (void)unityDidQuit:(NSNotification*)notification
+{
+    NSLog(@"unityDidQuit called");
+
+    [[self ufw] unregisterFrameworkListener: self];
+    [self setUfw: nil];
+    [self setDidQuit:true];
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application { [[[self ufw] appController] applicationWillResignActive: application]; }
+- (void)applicationDidEnterBackground:(UIApplication *)application { [[[self ufw] appController] applicationDidEnterBackground: application]; }
+- (void)applicationWillEnterForeground:(UIApplication *)application { [[[self ufw] appController] applicationWillEnterForeground: application]; }
+- (void)applicationDidBecomeActive:(UIApplication *)application { [[[self ufw] appController] applicationDidBecomeActive: application]; }
+- (void)applicationWillTerminate:(UIApplication *)application { [[[self ufw] appController] applicationWillTerminate: application]; }
 
 @end
--- Unity/opentok/holo/holo/main.m
+++ Unity/opentok/holo/holo/main.m
@@ -2,6 +2,8 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
+    gArgc = argc;
+    gArgv = argv;
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
