d616eaefa28e18cada5df1ca55baa982c6bee70a
Jose Antonio Olivera Ortega <joseantonio.oliveraortega@vonage.com>
2024-01-16 17:35:15 +0100

    OW-792: Build iOS test app

--- Unity/opentok/holo/holo/Capturer.h
+++ Unity/opentok/holo/holo/Capturer.h
@@ -12,4 +12,14 @@
     dispatch_queue_t _capture_queue;
 }
 
+@property (nonatomic, assign) NSString* captureSessionPreset;
+@property (readonly) NSArray* availableCaptureSessionPresets;
+
+@property (nonatomic, assign) double activeFrameRate;
+- (BOOL)isAvailableActiveFrameRate:(double)frameRate;
+
+@property (nonatomic, assign) AVCaptureDevicePosition cameraPosition;
+@property (readonly) NSArray* availableCameraPositions;
+- (BOOL)toggleCameraPosition;
+
 @end
