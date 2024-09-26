e304cedcffa1a3921bfd7ff7de09d8413a8755a9
Mike Thomas <mike.george.thomas@gmail.com>
2024-09-26 15:30:02 +0200

    oops ^

--- ML-Transformers/BackgroundEnchantments/copyToDocs.js
+++ ML-Transformers/BackgroundEnchantments/copyToDocs.js
@@ -3,6 +3,10 @@ const packageInfo = require('./package.json');
 
 const currentVersion = packageInfo.dependencies['@vonage/ml-transformers']
 
+if (currentVersion.includes('^') || currentVersion.includes('>') || currentVersion.includes('~')) {
+  throw Error('Use exact version for docs: https://docs.npmjs.com/cli/v8/configuring-npm/package-json#dependencies')
+}
+
 const folderName = '../../docs/ML-Transformers/BackgroundEnchantments/versions/' + currentVersion;
 fs.mkdirSync(folderName,  { recursive: true });
 fs.cpSync('./dist', folderName, {recursive: true});
